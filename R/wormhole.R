21d99623c1c0c2bb9da7bf4935d66ab31f4b02c9
Henrik Bengtsson <henrik.bengtsson@gmail.com>
2025-08-08 13:35:36 -0400

    tweaks

--- R/wormhole.R
+++ R/wormhole.R
@@ -63,13 +63,17 @@ wormhole_receive <- function(code, path = tempdir(), ..., rsh = NULL) {
   files
 }
 
-#' Find the Wormhole Executable
+#' Find a Wormhole Executable
 #'
 #' @return
 #' The absolute path to the `wormhole` executable as a character string.
 #' Attribute `version-string` comprise the `wormhole --version` output,
 #' and attributes `name` and `version` the parsed version string.
 #' If no executable exists, an error is produced.
+#'
+#' @details
+#' Unless R option `future.p2p.wormhole` specifies an executable, the default
+#' is to download and install `wormhole-william` locally and use that one.
 #'
 #' @export
 find_wormhole <- local({
